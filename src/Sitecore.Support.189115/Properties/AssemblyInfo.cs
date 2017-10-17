b47e5bce16f5d9d5b3dcf9391831af9fa6ea07c7
MichaelToh <ttk@sitecore.net>
2017-10-17 11:18:07 +0800

    To fix WFFM date picker display issue

---
+++ src/Sitecore.Support.189115/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.189115")]
+[assembly: AssemblyProduct("Sitecore.Support.189115")]
+[assembly: ComVisible(false)]
